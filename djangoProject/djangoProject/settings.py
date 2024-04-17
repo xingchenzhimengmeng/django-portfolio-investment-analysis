230471f2c0d6e619983fbb8b9827c050e61a9064
xingchenzhimengmeng <qin2390325216@qq.com>
2024-04-17 22:46:58 +0800

    第二次

--- djangoProject/djangoProject/settings.py
+++ djangoProject/djangoProject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^!4ts=6rthr9yf8mnoxsykz22!v*t)phua(ldkt=vvi7hbvre7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
